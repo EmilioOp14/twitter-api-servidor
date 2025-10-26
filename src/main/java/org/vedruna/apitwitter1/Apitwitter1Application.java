634f675ef1540baf75be33cdff6f5e51229119f7
EmilioOp14 <emilio.orduna@a.vedrunasevillasj.es>
2025-10-26 13:04:34 +0100

    First commit

---
+++ src/main/java/org/vedruna/apitwitter1/Apitwitter1Application.java
@@ -0,0 +1,13 @@
+package org.vedruna.apitwitter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apitwitter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apitwitter1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/vedruna/apitwitter1/Apitwitter1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vedruna.apitwitter1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apitwitter1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
